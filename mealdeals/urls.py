9988c3d7738c01e4d3172c239a1074342a9769b5
Justin Whittaker <just.whittaker@gmail.com>
2021-02-03 13:35:53 +0000

    feat(mealdeals): add favicon add logo

--- mealdeals/urls.py
+++ mealdeals/urls.py
@@ -17,9 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('media/img/favicon.png'))),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
