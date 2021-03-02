aac6c5b79742d2ec97dc4d53d15822eab517624a
Justin Whittaker <just.whittaker@gmail.com>
2021-03-02 12:03:52 +0000

    feat(mealdeals): client_profile = added templates add_deal.html and client_profile.html -- added views.py = add_deal

--- client_profile/views.py
+++ client_profile/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.db.models import Q
+from django.db.models.functions import Lower
+
+from .models import Deal, Category
+from .forms import ProductForm
 
 # Create your views here.
+
+
+def add_deal(request):
+    """ Add a deal """
+    form = DealForm()
+    template = 'client_profile/add_deal.html'
+    context = {
+        'form': form,
+    }
+
+return render(request, template, context)
--- mealdeals/settings.py
+++ mealdeals/settings.py
@@ -125,12 +125,6 @@ DATABASES = {
 #     }
 # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 # DATABASES['default'] = dj_database_url.config(
 #                         conn_max_age=600, ssl_require=True)
 
