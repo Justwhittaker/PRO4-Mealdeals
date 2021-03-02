6f3c136f7224f901a37e59359197e8b1cd7607b7
Justin Whittaker <just.whittaker@gmail.com>
2021-03-02 12:26:36 +0000

    feat(mealdeals): client_profile = added views.py = def client_deals and def deal_detail

--- client_profile/views.py
+++ client_profile/views.py
@@ -8,6 +8,63 @@ from .forms import ProductForm
 # Create your views here.
 
 
+def client_deals(request):
+    """ A veiw to show the specific clients delas in thier profile"""
+
+    deals = Deal.objects.all()
+    query = None
+    categories = None
+    sort = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+        sort = sortkey
+        if sortkey == 'name':
+            sortkey = 'lower_name'
+            deals == deals.annotate(lower_name=Lower('name'))
+        if sortkey == 'category':
+            sortkey = 'category_name'
+        deals = deals.order_by(sortkey)
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            deals = deals.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('index.html'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            deals = deals.filter(queries)
+
+    current_sorting = f'{sort}_{deals}'
+
+    context = {
+        'deals': deals,
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
+    }
+
+    return render(request, 'client_profile/client_profile.html', context)
+
+
+def deal_detail(request, deal_id):
+    """ A view to show individual deal details """
+
+    deals = get_object_or_404(Deal, pk=deal_id)
+
+    context = {
+        'deals': deals,
+    }
+
+    return render(request, 'client_profile/deal_detail.html', context)
+
+
 def add_deal(request):
     """ Add a deal """
     form = DealForm()
@@ -16,4 +73,4 @@ def add_deal(request):
         'form': form,
     }
 
-return render(request, template, context)
+    return render(request, template, context)
