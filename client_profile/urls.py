db20a035b59a9d4e97f31e712371e8d246b4a4e7
Justin Whittaker <just.whittaker@gmail.com>
2021-03-20 14:25:05 +0000

    fix(mealdeals):build error in Heroku due to Database error not wired in with if statement

--- client_profile/urls.py
+++ client_profile/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.client_deals, name='deals'),
-    path('<int:deal_id>/', views.deal_detail, name='deal_detail'),
     path('add/', views.add_deal, name='add_deal'),
 ]
--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.all_deals, name="home"),
     path('privacy/', views.privacy, name="privacy"),
-    path('about/', views.about, name="about")
+    path('about/', views.about, name="about"),
 ]
