81492dee73b78ebb67760bc9cedac7fdef8070aa
Justin Whittaker <just.whittaker@gmail.com>
2021-03-20 13:20:28 +0000

    feat(mealdeals): deals_display == create add deal.html copied deal display as placeholder

--- deals_display/urls.py
+++ deals_display/urls.py
@@ -1,6 +1,8 @@
-# from django.urls import path
-# from . import views
+from django.urls import path
+from . import views
 
-# urlpatterns = [
-#     path('', views.all_products, name='products')
-# ]
+urlpatterns = [
+    path('', views.all_deals, name="deals_display"),
+    path('<int:deal_id>/', views.deal_detail, name='deal_detail'),
+    path('add/', views.add_deal, name='add_deal'),
+]
