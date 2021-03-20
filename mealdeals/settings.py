f6c00b0500c366338eed25f2071f93833df68925
Justin Whittaker <just.whittaker@gmail.com>
2021-03-20 14:50:05 +0000

    fix(mealdeals):build error in Heroku due to Database error not wired in with if statement

--- mealdeals/settings.py
+++ mealdeals/settings.py
@@ -109,12 +109,12 @@ LOGIN_REDIRECT_URL = '/'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
+# if 'DATABASE_URL' in os.environ:
+#     DATABASES = {
+#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#     }
+# else:
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
