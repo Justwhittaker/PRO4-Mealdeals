5befd61aa9b011b5551d797c3a98a5c1736738d5
Justin Whittaker <just.whittaker@gmail.com>
2021-03-20 14:48:38 +0000

    fix(mealdeals):build error in Heroku due to Database error not wired in with if statement

--- mealdeals/settings.py
+++ mealdeals/settings.py
@@ -135,7 +135,7 @@ else:
 # DATABASES['default'] = dj_database_url.config(
 #                         conn_max_age=600, ssl_require=True)
 
-DATABASE_URL = os.environ['DATABASE_URL']
+# DATABASE_URL = os.environ['DATABASE_URL']
 
 # conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
