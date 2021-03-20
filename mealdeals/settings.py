2027c459f83e44c33b1a2dc1b9f3ee26307313f2
Justin Whittaker <just.whittaker@gmail.com>
2021-03-20 16:29:05 +0000

    fix(mealdeals): fixed requirements.txt cleared after reinstalling gunicorn refroze

--- mealdeals/settings.py
+++ mealdeals/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dj=v=!s+lpj#4xkmr)0x7!zs3i%@yed!e_8cq7sd--4i%ah%fa'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
