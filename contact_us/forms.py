e41f6600b84b78ceec985df6b7c9bce7d5fe4543
Justin Whittaker <just.whittaker@gmail.com>
2021-03-23 01:27:15 +0000

    style(mealdeals): contact.html == styled forms to have placeholders and css padding

--- contact_us/forms.py
+++ contact_us/forms.py
@@ -8,4 +8,29 @@ class ContactForm(forms.Form):
     last_name = forms.CharField(max_length=45)
     restaurant_name = forms.CharField(max_length=200)
     email_address = forms.EmailField(max_length=200)
+    phone_number = forms.CharField(max_length=45)
     deal_description = forms.CharField(widget=forms.Textarea, max_length=2000)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email_address': 'Email Address',
+            'phone_number': 'Phone Number',
+            'restaurant_name': 'Restaurant Name',
+            'deal_description': 'Deal Description',
+        }
+
+        self.fields['restaurant_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False
