389f20cc1b7fd91aa8dce2657d6d273d377b7663
Justin Whittaker <just.whittaker@gmail.com>
2021-03-27 11:39:48 +0000

    feat(mealdeals): added pip install pylint-django to assit with linting problems on my project

--- client_profile/views.py
+++ client_profile/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
