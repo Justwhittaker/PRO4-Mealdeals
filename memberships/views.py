2f7e6d07adcc30a65692537762d259bcdccfe896
Justin Whittaker <just.whittaker@gmail.com>
2021-03-31 11:01:59 +0000

    refactor(mealdeals):refactor the URL for success and cancel for stripe payments

--- memberships/views.py
+++ memberships/views.py
@@ -77,7 +77,9 @@ def checkout(request):
             }],
             mode='subscription',
             allow_promotion_codes=True,
-            success_url=(f"https://mealdeals-pro.herokuapp.com/success?session_id={CHECKOUT_SESSION_ID}"),
+            success_url=(
+                f"{'https://mealdeals-pro.herokuapp.com/success?session_id={CHECKOUT_SESSION_ID}'}"
+                ),
             cancel_url="https://mealdeals-pro.herokuapp.com/cancel"
         )
 
